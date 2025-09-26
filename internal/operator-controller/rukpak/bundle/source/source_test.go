0aae47834fe3484444e2e592540cd537c9a5fb66
Per Goncalves da Silva <pegoncal@redhat.com>
2025-09-26 17:12:55 +0200

    Improve registry+v1 bundle loading from filesystem
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- internal/operator-controller/rukpak/bundle/source/source_test.go
+++ internal/operator-controller/rukpak/bundle/source/source_test.go
@@ -1,75 +1,104 @@
 package source_test
 
 import (
-	"io/fs"
-	"strings"
-	"testing"
-	"testing/fstest"
+    "io/fs"
+    "testing"
 
-	"github.com/stretchr/testify/require"
+    "github.com/operator-framework/api/pkg/operators/v1alpha1"
+    registry "github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/operator-registry"
+    "github.com/stretchr/testify/require"
+    corev1 "k8s.io/api/core/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/utils/ptr"
 
-	"github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/bundle"
-	"github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/bundle/source"
-	. "github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/util/testing"
+    "github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/bundle"
+    "github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/bundle/source"
+    . "github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/util/testing"
 )
 
 const (
-	olmProperties = "olm.properties"
+    olmProperties = "olm.properties"
 )
 
 func Test_FromBundle_Success(t *testing.T) {
-	expectedBundle := bundle.RegistryV1{
-		PackageName: "my-package",
-	}
-	b, err := source.FromBundle(expectedBundle).GetBundle()
-	require.NoError(t, err)
-	require.Equal(t, expectedBundle, b)
+    expectedBundle := bundle.RegistryV1{
+        PackageName: "my-package",
+    }
+    b, err := source.FromBundle(expectedBundle).GetBundle()
+    require.NoError(t, err)
+    require.Equal(t, expectedBundle, b)
 }
 
 func Test_FromFS_Success(t *testing.T) {
-	rv1, err := source.FromFS(NewBundleFS()).GetBundle()
-	require.NoError(t, err)
+    bundleFS := NewBundleFS(
+        WithBundleAnnotations(registry.Annotations{PackageName: "test"}),
+        WithBundleProperty("from-file-key", "from-file-value"),
+        WithBundleResource("csv.yaml", ptr.To(MakeCSV(
+            WithName("test.v1.0.0"),
+            WithAnnotations(map[string]string{
+                "olm.properties": `[{"type":"from-csv-annotations-key", "value":"from-csv-annotations-value"}]`,
+            }),
+            WithInstallModeSupportFor(v1alpha1.InstallModeTypeAllNamespaces)),
+        )),
+    )
 
-	t.Log("Check package name is correctly taken from metadata/annotations.yaml")
-	require.Equal(t, "test", rv1.PackageName)
+    rv1, err := source.FromFS(bundleFS).GetBundle()
+    require.NoError(t, err)
 
-	t.Log("Check metadata/properties.yaml is merged into csv.annotations[olm.properties]")
-	require.JSONEq(t, `[{"type":"from-csv-annotations-key","value":"from-csv-annotations-value"},{"type":"from-file-key","value":"from-file-value"}]`, rv1.CSV.Annotations[olmProperties])
+    t.Log("Check package name is correctly taken from metadata/annotations.yaml")
+    require.Equal(t, "test", rv1.PackageName)
+
+    t.Log("Check metadata/properties.yaml is merged into csv.annotations[olm.properties]")
+    require.JSONEq(t, `[{"type":"from-csv-annotations-key","value":"from-csv-annotations-value"},{"type":"from-file-key","value":"from-file-value"}]`, rv1.CSV.Annotations[olmProperties])
 }
 
 func Test_FromFS_Fails(t *testing.T) {
-	for _, tt := range []struct {
-		name string
-		FS   fs.FS
-	}{
-		{
-			name: "bundle missing ClusterServiceVersion manifest",
-			FS:   removePaths(NewBundleFS(), BundlePathCSV),
-		}, {
-			name: "bundle missing metadata/annotations.yaml",
-			FS:   removePaths(NewBundleFS(), BundlePathAnnotations),
-		}, {
-			name: "bundle missing metadata/ directory",
-			FS:   removePaths(NewBundleFS(), "metadata/"),
-		}, {
-			name: "bundle missing manifests/ directory",
-			FS:   removePaths(NewBundleFS(), "manifests/"),
-		},
-	} {
-		t.Run(tt.name, func(t *testing.T) {
-			_, err := source.FromFS(tt.FS).GetBundle()
-			require.Error(t, err)
-		})
-	}
-}
-
-func removePaths(mapFs fstest.MapFS, paths ...string) fstest.MapFS {
-	for k := range mapFs {
-		for _, path := range paths {
-			if strings.HasPrefix(k, path) {
-				delete(mapFs, k)
-			}
-		}
-	}
-	return mapFs
+    for _, tt := range []struct {
+        name           string
+        FS             fs.FS
+        expectedErrMsg string
+    }{
+        {
+            name: "bundle missing ClusterServiceVersion manifest",
+            FS: NewBundleFS(
+                WithBundleAnnotations(registry.Annotations{PackageName: "test"}),
+                WithBundleProperty("foo", "bar"),
+                WithBundleResource("service.yaml", &corev1.Service{
+                    TypeMeta: metav1.TypeMeta{
+                        Kind:       "Service",
+                        APIVersion: corev1.SchemeGroupVersion.String(),
+                    },
+                }),
+            ),
+            expectedErrMsg: "no ClusterServiceVersion found in \"manifests\"",
+        }, {
+            name: "bundle missing metadata/annotations.yaml",
+            FS: NewBundleFS(
+                WithBundleProperty("foo", "bar"),
+                WithBundleResource("csv.yaml", ptr.To(MakeCSV())),
+            ),
+            expectedErrMsg: "malformed bundle: required file metadata/annotations.yaml not found",
+        }, {
+            name: "metadata/annotations.yaml missing package name annotation",
+            FS: NewBundleFS(
+                WithBundleAnnotations(registry.Annotations{}),
+                WithBundleProperty("foo", "bar"),
+                WithBundleResource("csv.yaml", ptr.To(MakeCSV())),
+            ),
+            expectedErrMsg: "malformed bundle: operators.operatorframework.io.bundle.package.v1 annotation not found in metadata/annotations.yaml",
+        }, {
+            name: "bundle missing manifests directory",
+            FS: NewBundleFS(
+                WithBundleAnnotations(registry.Annotations{PackageName: "test"}),
+                WithBundleProperty("foo", "bar"),
+            ),
+            expectedErrMsg: "malformed bundle: manifests directory not found",
+        },
+    } {
+        t.Run(tt.name, func(t *testing.T) {
+            _, err := source.FromFS(tt.FS).GetBundle()
+            require.Error(t, err)
+            require.Contains(t, err.Error(), tt.expectedErrMsg)
+        })
+    }
 }
